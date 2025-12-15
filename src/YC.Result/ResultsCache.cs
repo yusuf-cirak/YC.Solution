3a901bdf7dbc95fe9ad8009adffab2432e174e74
Yusuf Çırak <yusuf10901@hotmail.com>
2025-12-15 21:08:58 +0300

    chore: remove YC.Result

--- src/YC.Result/ResultsCache.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace YC.Result;
-
-internal static class ResultsCache
-{
-    internal static Result Success { get; } = new(true);
-    internal static Result Failure { get; } = new(false);
-}
